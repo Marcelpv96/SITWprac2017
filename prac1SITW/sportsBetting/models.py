f475f6232faa8158c14fce97fe353be5bd615597
Guillem <guillem.orellana@gmail.com>
2017-04-10 13:49:57 +0200

    models added

--- prac1SITW/sportsBetting/models.py
+++ prac1SITW/sportsBetting/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Sport(models.Model):
+    name = models.TextField()
+
+class Event(models.Model):
+    name    = models.TextField()
+    sport   = models.ForeingKey(Sport)
+    local   = models.ForeingKey(Team)
+    visitor = models.ForeingKey(Team)
+
+class Team(models.Model):
+    name = models.TextField()
+    country = models.TextField()
+
+class Bet(models.Model):
+    user = models.ForeingKey(User)
+    event = models.ForeingKey(Event)
